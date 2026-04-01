8eb511884d3b7ebc8a78a3018d8e0d96adf03a97
bakri090 <bakri090200@gmail.com>
2026-04-01 17:03:14 +0300

    start backend

---
+++ Backend/WepApi/Controllers/CityController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WepApi.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class CityController : ControllerBase
+{
+	[HttpGet]
+	public IActionResult getAll()
+	{
+		string[] cities = { "Atalanta", "New York" };
+		return Ok(cities);
+	}
+}
